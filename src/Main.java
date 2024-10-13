d1fd77e5bfec7f0c1740accd1e6274ad53a99f04
Eyiram Gaze <eyiramgaze@gmail.com>
2024-10-13 12:13:30 +0000

    Changed code in Main class with refactored classes and methods

--- src/Main.java
+++ src/Main.java
@@ -15,32 +15,61 @@ public class Main {
         System.out.print(Country.countryWithHighestCH4InYear(countries, 2000).getName() + " : ");
         System.out.print(Country.countryWithHighestCH4InYear(countries, 2000).getEmissions().get(2000).getCH4() + " kilotons" + "\n\n");
 
-        /*Which country had the highest increase in greenhouse gas emissions between 1988 (the year the
-        Intergovernmental Panel on Climate Change was formed) and 2012?
-         */
-        System.out.println("Country with the highest increase in greenhouse gas emissions between 1988 and 2012:");
-        System.out.print(Country.highestChangeInEmissions(countries, 1988, 2012).getName() + " : ");
-        double chinaCH4Emission1988 = Country.highestChangeInEmissions(countries, 1988, 2012).getEmissions().get(1988).getCH4();
-        double chinaN2OEmission1988 = Country.highestChangeInEmissions(countries, 1988, 2012).getEmissions().get(1988).getN2O();
-        double chinaCO2Emission1988 = Country.highestChangeInEmissions(countries, 1988, 2012).getEmissions().get(1988).getCO2();
-        double totalChinaEmission1988 = chinaCO2Emission1988 + chinaCH4Emission1988 + chinaN2OEmission1988;
+        /*Country With Highest Change in Emission between 1970 and 2000 */
+        System.out.println("Country with the highest change in emissions 1970 and 2000:");
+        System.out.print(Country.highestChangeInEmissions(countries, 1970, 2000).getName() + " : ");
+        double chinaCH4Emission1970 = Country.highestChangeInEmissions(countries, 1970, 2000).getEmissions().get(1970).getCH4();
+        double chinaCO2Emission1970 = Country.highestChangeInEmissions(countries, 1970, 2000).getEmissions().get(1970).getCO2();
+        double chinaN2OEmission1970 = Country.highestChangeInEmissions(countries, 1970, 2000).getEmissions().get(1970).getN2O();
+        double totalChinaEmission1970 = chinaCO2Emission1970 + chinaCH4Emission1970 + chinaN2OEmission1970;
 
-        double chinaCH4Emission2012 = Country.highestChangeInEmissions(countries, 1988, 2012).getEmissions().get(2012).getCH4();
-        double chinaN2OEmission2012 = Country.highestChangeInEmissions(countries, 1988, 2012).getEmissions().get(2012).getN2O();
-        double chinaCO2Emission2012 = Country.highestChangeInEmissions(countries, 1988, 2012).getEmissions().get(2012).getCO2();
-        double totalChinaEmission2012 = chinaCO2Emission2012 + chinaCH4Emission2012 + chinaN2OEmission2012;
-        System.out.print(totalChinaEmission2012 - totalChinaEmission1988 + " kilotons" + "\n");
+        double chinaCH4Emission2000 = Country.highestChangeInEmissions(countries, 1970, 2000).getEmissions().get(2000).getCH4();
+        double chinaN2OEmission2000 = Country.highestChangeInEmissions(countries, 1970, 2000).getEmissions().get(2000).getN2O();
+        double chinaCO2Emission2000 = Country.highestChangeInEmissions(countries, 1970, 2000).getEmissions().get(2000).getCO2();
+        double totalChinaEmission2000 = chinaCO2Emission2000 + chinaCH4Emission2000 + chinaN2OEmission2000;
+        System.out.print(totalChinaEmission2000 - totalChinaEmission1970 + " kilotons" + "\n");
 
-        /* Which sector had the
-        highest change in greenhouse gas emissions between 1988 and 2012?
+        /* Which sector had the highest change in greenhouse gas emissions between 1970 and 2000?
         */
-        double powerIndustryEmission1988 = Sector.highestChangeInEmissions(sectors, 1988, 2012).getEmissions().get(1988);
-        double powerIndustryEmission2012 = Sector.highestChangeInEmissions(sectors, 1988, 2012).getEmissions().get(2012);
+        double powerIndustryEmission1970 = Sector.highestChangeInEmissions(sectors, 1970, 2000).getEmissions().get(1970);
+        double powerIndustryEmission2000 = Sector.highestChangeInEmissions(sectors, 1970, 2000).getEmissions().get(2000);
 
-        System.out.print("\nSector with the highest change in greenhouse gas emission between 1988 and 2012: ");
-        double powerIndustryChange = powerIndustryEmission2012 - powerIndustryEmission1988;
-        System.out.print("\n" + Sector.highestChangeInEmissions(sectors, 1988, 2012).getName() + " : ");
-        System.out.print(powerIndustryChange + " kilotons" + "\n");
+        System.out.print("\nSector with the highest change in greenhouse gas emission between 1970 and 2000: ");
+        double powerIndustryChange = powerIndustryEmission2000 - powerIndustryEmission1970;
+        System.out.print("\n" + Sector.highestChangeInEmissions(sectors, 1970, 2000).getName() + " : ");
+        System.out.println(powerIndustryChange + " kilotons" + "\n");
+
+        // Which country had the highest methane gas emissions in 2000?
+        System.out.println("Country with the highest methane gas emissions in 2012:");
+        System.out.print(Country.countryWithHighestCH4InYear(countries, 2012).getName() + " : ");
+        System.out.print(Country.countryWithHighestCH4InYear(countries, 2012).getEmissions().get(2012).getCH4() + " kilotons" + "\n\n");
+
+        /*Country With Highest Change in Emission between 1970 and 2000 */
+        System.out.println("Country with the highest change in emissions 2000 and 2012:");
+        System.out.print(Country.highestChangeInEmissions(countries, 2000, 2012).getName() + " : ");
+        double countryCH4Emission2000 = Country.highestChangeInEmissions(countries, 2000, 2012).getEmissions().get(2000).getCH4();
+        double countryCO2Emission2000 = Country.highestChangeInEmissions(countries, 2000, 2012).getEmissions().get(2000).getCO2();
+        double countryN2OEmission2000 = Country.highestChangeInEmissions(countries, 2000, 2012).getEmissions().get(2000).getN2O();
+        double totalCountryEmission2000 = countryCO2Emission2000 + countryCH4Emission2000 + countryN2OEmission2000;
+
+        double countryCH4Emission2012 = Country.highestChangeInEmissions(countries, 2000, 2012).getEmissions().get(2012).getCH4();
+        double countryN2OEmission2012 = Country.highestChangeInEmissions(countries, 2000, 2012).getEmissions().get(2012).getN2O();
+        double countryCO2Emission2012 = Country.highestChangeInEmissions(countries, 2000, 2012).getEmissions().get(2012).getCO2();
+        double totalCountryEmission2012 = countryCO2Emission2012 + countryCH4Emission2012 + countryN2OEmission2012;
+        System.out.print(totalCountryEmission2012 - totalCountryEmission2000 + " kilotons" + "\n");
+
+
+        /* Sector with the highest change in greenhouse gas emission between 2005 and 2012 */
+        double sectorEmission2005 = Sector.highestChangeInEmissions(sectors, 2005, 2012).getEmissions().get(2005);
+        double sectorEmission2012 = Sector.highestChangeInEmissions(sectors, 2005, 2012).getEmissions().get(2012);
+
+        System.out.print("\nSector with the highest change in greenhouse gas emission between 2005 and 2012: ");
+        double sectorChange = sectorEmission2012 - sectorEmission2005;
+
+        System.out.print("\n" + Sector.highestChangeInEmissions(sectors, 2005, 2012).getName() + " : ");
+        System.out.println(sectorChange + " kilotons" + "\n");
+
+
     }
 
 	/**
